48c5f3c3ab534e0c264b8ce51ea0b39b3a91bc15
wanhelaa <elanurislam532@gmail.com>
2025-10-02 12:36:59 +0300

    Eklendi: 2. Hafta Sürüklenen buton

---
+++ görsel/gorsel programlama 2/hafta 2/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace hafta_2_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(522, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Arka kırmızı";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(522, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "yazı sarı";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            this.button2.MouseHover += new System.EventHandler(this.button2_MouseHover);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(522, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "font +5";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button3_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ela nur ";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(247, 331);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(207, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "kaçan kovalanaır";
+            this.button4.UseVisualStyleBackColor = true;
+           
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(307, 394);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "sürükle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button5_MouseDown);
+            this.button5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button5_MouseMove);
+            this.button5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button5_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 470);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ görsel/gorsel programlama 2/hafta 2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hafta_2_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.BackColor = Color.DeepPink; //moosu butona getirince
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Yellow; 
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Blue;
+        }
+
+        private void button3_MouseDown(object sender, MouseEventArgs e)
+        {
+            int sayi = 0;  //font büyütür 
+            sayi = Convert.ToInt32(label1.Font.Size); sayi += 5;
+            label1.Font = new Font("Arial", sayi);
+        }
+
+        bool surukleniyor = false;
+        Point baslangicNoktasi = new Point(0, 0);
+        private void button5_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) // Sol tuş ile sürükleme başlasın
+            {
+                surukleniyor = true;
+                baslangicNoktasi = e.Location; // tıklandığı noktayı kaydet
+            }
+        }
+
+        private void button5_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (surukleniyor)
+            {
+                button5.Left += e.X - baslangicNoktasi.X;
+                button5.Top += e.Y - baslangicNoktasi.Y;
+            }
+        }
+        private void button5_MouseUp(object sender, MouseEventArgs e)
+        {
+            surukleniyor = false; // sürükleme bitti
+        }
+    }
+}
